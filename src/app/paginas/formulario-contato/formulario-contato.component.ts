aff6ca5fd81f7811995733a046f2168f2e5f66e1
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-06 10:20:48 -0300

    first commit

---
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-contato',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './formulario-contato.component.html',
+  styleUrl: './formulario-contato.component.css'
+})
+export class FormularioContatoComponent {
+  contatoForm!: FormGroup;
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Nay'),
+      telefone: new FormControl('92 99999-9999'),
+      email: new FormControl('asdf@asdf.com'),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      obs: new FormControl('')
+    });
+  }
+
+  salvarContato() {
+    console.log(this.contatoForm.value);
+  }
+
+  cancelar() {
+    console.log('Submissão cancelada');
+  }
+}
