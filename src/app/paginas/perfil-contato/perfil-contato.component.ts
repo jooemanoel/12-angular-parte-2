b72c4d2c45ef1d6bb05007dca8355abd8f2fcd0d
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-07 11:15:07 -0300

    Final da aula 2

---
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from "../../componentes/container/container.component";
+import { Contato } from '../../componentes/contato/contato';
+
+@Component({
+  selector: 'app-perfil-contato',
+  standalone: true,
+  imports: [CommonModule, ContainerComponent],
+  templateUrl: './perfil-contato.component.html',
+  styleUrl: './perfil-contato.component.css'
+})
+export class PerfilContatoComponent {
+  contato: Contato = {
+    id: 0,
+    nome: 'dev',
+    telefone: '12123451234',
+    email: 'dev@email.com',
+    aniversario: '12/10/1990',
+    redes: 'dev.com'
+  };
+
+}
