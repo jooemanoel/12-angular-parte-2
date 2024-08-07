a8c3800775487b42e88b2f302b66836db0073ec1
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-07 14:53:18 -0300

    Final da aula 4

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -36,6 +36,7 @@ export class FormularioContatoComponent implements OnInit {
   inicializarFormulario() {
     this.contatoForm = new FormGroup({
       nome: new FormControl('', Validators.required),
+      avatar: new FormControl('', Validators.required),
       telefone: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
@@ -58,13 +59,28 @@ export class FormularioContatoComponent implements OnInit {
       const novoContato = this.contatoForm.value;
       const id = this.activatedRoute.snapshot.paramMap.get('id');
       novoContato.id = id ? parseInt(id) : null;
-      console.log(novoContato);
       this.contatosService.salvarContato(novoContato).subscribe(() => {
         this.contatoForm.reset();
         this.router.navigateByUrl('/lista-contatos');
       });
-
     }
+  }
+
+  aoSelecionarArquivo(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.lerArquivo(file);
+    }
+  }
+
+  lerArquivo(file: File) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.result) {
+        this.contatoForm.get('avatar')?.setValue(reader.result);
+      }
+    }
+    reader.readAsDataURL(file);
   }
 
   cancelar() {
--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -4,16 +4,25 @@ import { ContainerComponent } from "../../componentes/container/container.compon
 import { Contato } from '../../componentes/contato/contato';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ContatoService } from '../../services/contato.service';
+import { SeparadorComponent } from "../../componentes/separador/separador.component";
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
-  imports: [CommonModule, ContainerComponent, RouterLink],
+  imports: [CommonModule, ContainerComponent, RouterLink, SeparadorComponent],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
 })
 export class PerfilContatoComponent implements OnInit {
-  contato: Contato = { id: 0, nome: '', telefone: '', email: '', aniversario: '', redes: '' };
+  contato: Contato = {
+    id: 0,
+    nome: '',
+    avatar: '',
+    telefone: '',
+    email: '',
+    aniversario: '',
+    redes: ''
+  };
 
   constructor(
     private contatosService: ContatoService,
